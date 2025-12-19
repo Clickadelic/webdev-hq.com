1231bbc22787d38c474f3f583d81c6bb5d2c01a5
Clickadelic <toby.hopp@gmail.com>
2025-12-19 22:24:24 +0100

    hover bug fix

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,13 +3,18 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-8 items-center justify-center">
+                <AppLogoIcon />
+                {/* <img src="./icons/extension/icon-128.png" className="bg-white group-hover:opacity-80" width="32" height="32" alt="WebDev HQ Logo" /> */}
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
-                </span>
+                <h1 className="text-2xl my-3 max-w-[180px]">
+                    <div className="font-light flex hover:text-slate-500" id="logo-text">
+                        <span className="web">Web</span>
+                        <span className="font-medium mr-2">Dev</span>
+                        <span className="font-medium">HQ</span>
+                    </div>
+                </h1>
             </div>
         </>
     );
