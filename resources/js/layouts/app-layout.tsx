45c649948b21fc0dc6a7012a34d4a4f039fa242b
Clickadelic <toby.hopp@gmail.com>
2026-02-23 20:12:07 +0100

    Toaster added

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -2,6 +2,10 @@ import { Head } from '@inertiajs/react';
 import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
 import { type BreadcrumbItem } from '@/types';
 import { type ReactNode } from 'react';
+
+import { Toaster } from "@/components/ui/sonner"
+
+
 interface AppLayoutProps {
     children: ReactNode;
     breadcrumbs?: BreadcrumbItem[];
@@ -11,5 +15,6 @@ export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
     <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
         <Head title={breadcrumbs?.[breadcrumbs.length - 1]?.title} />
         {children}
+        <Toaster />
     </AppLayoutTemplate>
 );
