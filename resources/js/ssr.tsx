0829eb9f4b7669262564e9f09ab083fcc78ea74a
Clickadelic <toby.hopp@gmail.com>
2026-02-28 03:26:08 +0100

    middot

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -9,7 +9,7 @@ createServer((page) =>
     createInertiaApp({
         page,
         render: ReactDOMServer.renderToString,
-        title: (title) => (title ? `${title} - ${appName}` : appName),
+        title: (title) => (title ? `${title} &middot; ${appName}` : appName),
         resolve: (name) =>
             resolvePageComponent(
                 `./pages/${name}.tsx`,
