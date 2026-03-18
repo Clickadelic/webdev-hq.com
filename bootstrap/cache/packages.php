e01c2fea463dfe69605ae2b1faa84fd352aad5b6
Clickadelic <toby.hopp@gmail.com>
2026-03-18 03:22:00 +0100

    sanctum installed

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -70,6 +70,13 @@
       0 => 'Laravel\\Sail\\SailServiceProvider',
     ),
   ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
   'laravel/surveyor' => 
   array (
     'providers' => 
