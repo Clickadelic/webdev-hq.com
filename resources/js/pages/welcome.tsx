5f9b650e7316bfbb5faa760acdd61b37b762a3a9
Clickadelic <toby.hopp@gmail.com>
2026-05-12 21:58:55 +0200

    HMR changes

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -14,9 +14,9 @@ export default function Welcome({
 }) {
     return (
         <PublicLayout canRegister={canRegister} title="Welcome">
-            <div className="my-8 rounded-2xl bg-cyan-400 p-8 text-center text-slate-950 shadow-lg">
+            <div className="my-8 rounded-2xl bg-amber-400 p-8 text-center text-slate-950 shadow-lg">
                 <p className="mb-2 text-sm font-semibold tracking-[0.3em] uppercase">
-                    HMR Working! 🚀
+                    HMR Working, yeehaw! 🚀
                 </p>
                 <h2 className="text-center text-3xl font-medium">Suche</h2>
             </div>
