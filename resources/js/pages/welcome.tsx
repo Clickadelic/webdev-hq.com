76793a31813add1b428134243b41d0c8b9f568c3
Clickadelic <toby.hopp@gmail.com>
2026-05-16 05:20:19 +0200

    App Strict mode

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { CategoryComboBox } from '@/components/forms/category-combobox';
 import PublicLayout from '@/layouts/public-layout';
 import { type Hyperlink } from '@/types';
 
@@ -15,6 +16,14 @@ export default function Welcome({
             <div className="my-3 rounded-lg border border-amber-400 bg-white/30 p-1 backdrop-blur">
                 <h2 className="text-center font-medium">Suche</h2>
             </div>
+            <CategoryComboBox categories={hyperlinks} />
+            {hyperlinks.length === 0 && (
+                <div className="my-3 rounded-lg border border-amber-400 bg-white/30 p-1 backdrop-blur">
+                    <h2 className="text-center font-medium">
+                        Keine Kategorien gefunden
+                    </h2>
+                </div>
+            )}
         </PublicLayout>
     );
 }
