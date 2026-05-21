c6f114e376de288b54e9318c9984c86cc2bd0d3d
Clickadelic <toby.hopp@gmail.com>
2026-05-21 03:50:38 +0200

    Public header style

--- resources/js/components/public-header.tsx
+++ resources/js/components/public-header.tsx
@@ -25,8 +25,8 @@ export default function PublicHeader({
             )}
         >
             <header className="container mx-auto flex items-center justify-between">
-                <AppLogo />
-                <nav className="flex w-full items-center justify-center gap-4">
+                <AppLogo className="lg:mr-24" />
+                <nav className="flex w-full items-center justify-between gap-4">
                     <ul className="flex items-start gap-4">
                         <li>
                             <Link href="/" className="font-medium" title="Home">
@@ -36,37 +36,39 @@ export default function PublicHeader({
                         <li>
                             <Link href="/hyperlinks">Hyperlinks</Link>
                         </li>
-                        <li>
-                            <Link href="#">Link</Link>
-                        </li>
-                        <li>
-                            <Link href="#">Link</Link>
-                        </li>
                     </ul>
                     {auth.user ? (
-                        <Link
-                            href={dashboard()}
-                            className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                        >
-                            Dashboard
-                        </Link>
-                    ) : (
-                        <>
-                            <Link
-                                href={login()}
-                                className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                            >
-                                Log in
-                            </Link>
-                            {canRegister && (
+                        <ul className="flex-end flex items-center gap-4">
+                            <li>
                                 <Link
-                                    href={register()}
+                                    href={dashboard()}
                                     className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                                 >
-                                    Register
+                                    Dashboard
                                 </Link>
+                            </li>
+                        </ul>
+                    ) : (
+                        <ul className="flex-end flex items-center gap-4">
+                            <li>
+                                <Link
+                                    href={login()}
+                                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                                >
+                                    Log in
+                                </Link>
+                            </li>
+                            {canRegister && (
+                                <li>
+                                    <Link
+                                        href={register()}
+                                        className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                                    >
+                                        Register
+                                    </Link>
+                                </li>
                             )}
-                        </>
+                        </ul>
                     )}
                 </nav>
             </header>
