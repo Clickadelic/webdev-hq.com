1e5dfbdcc201aa9dc34e939a12f9ee59084ad5b3
Clickadelic <toby.hopp@gmail.com>
2026-06-01 17:35:48 +0200

    linktitles

--- resources/js/pages/legal/index.tsx
+++ resources/js/pages/legal/index.tsx
@@ -12,11 +12,27 @@ export default function LegalIndex({
                     Legal Information
                 </h2>
                 <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
-                    <Link href="/legal/disclaimer">Disclaimer</Link>
-                    <Link href="/legal/cookie-policy">Cookie Policy</Link>
-                    <Link href="/legal/cookie-policy">Cookie Policy</Link>
-                    <Link href="/legal/cookie-policy">Cookie Policy</Link>
-                    <Link href="/legal/cookie-policy">Cookie Policy</Link>
+                    <Link href="/legal/disclaimer" title="Disclaimer">
+                        Disclaimer
+                    </Link>
+                    <Link href="/legal/cookie-policy" title="Cookie Policy">
+                        Cookie Policy
+                    </Link>
+                    <Link
+                        href="/legal/terms-of-service"
+                        title="Terms of Service"
+                    >
+                        Terms of Service
+                    </Link>
+                    <Link href="/legal/terms-of-use" title="Terms of use">
+                        Terms of use
+                    </Link>
+                    <Link
+                        href="/legal/terms-of-privacy"
+                        title="Terms of privacy"
+                    >
+                        Terms of privacy
+                    </Link>
                 </div>
             </div>
         </PublicSidebarLeftLayout>
