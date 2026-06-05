347520a94fea3bdfabe48eb40130732c0676f818
Clickadelic <toby.hopp@gmail.com>
2026-06-05 02:20:12 +0200

    Details, details

--- resources/js/components/public-footer.tsx
+++ resources/js/components/public-footer.tsx
@@ -9,17 +9,17 @@ export default function PublicFooter({ className }: PublicFooterProps) {
     return (
         <footer
             className={cn(
-                'w-full bg-white shadow dark:bg-neutral-900',
+                'w-full border-t-2 border-primary bg-neutral-900 px-3 py-6 sm:px-0',
                 className,
             )}
         >
-            <div className="container mx-auto py-6">
+            <div className="container mx-auto pt-12 pb-8">
                 <div className="flex flex-row items-center justify-center">
                     <ul className="mx-auto mb-4 space-y-3 text-center text-sm sm:flex sm:space-y-0 sm:space-x-3">
                         <li>
                             <Link
                                 href="/legal/disclaimer"
-                                className="text-sm hover:text-primary"
+                                className="text-sm text-neutral-200 hover:text-primary dark:text-neutral-200"
                                 title="Disclaimer"
                             >
                                 Disclaimer
@@ -29,7 +29,7 @@ export default function PublicFooter({ className }: PublicFooterProps) {
                         <li>
                             <Link
                                 href="/legal/cookie-policy"
-                                className="text-sm hover:text-primary"
+                                className="text-sm text-neutral-200 hover:text-primary dark:text-neutral-200"
                                 title="Cookie Policy"
                             >
                                 Cookie Policy
@@ -40,7 +40,7 @@ export default function PublicFooter({ className }: PublicFooterProps) {
                         <li>
                             <Link
                                 href="/legal/legal-notice"
-                                className="text-sm hover:text-primary"
+                                className="text-sm text-neutral-200 hover:text-primary dark:text-neutral-200"
                                 title="Legal Notice"
                             >
                                 Legal Notice
@@ -50,7 +50,7 @@ export default function PublicFooter({ className }: PublicFooterProps) {
                         <li>
                             <Link
                                 href="/legal/privacy-policy"
-                                className="text-sm hover:text-primary"
+                                className="text-sm text-neutral-200 hover:text-primary dark:text-neutral-200"
                                 title="Privacy Policy"
                             >
                                 Privacy Policy
@@ -60,7 +60,7 @@ export default function PublicFooter({ className }: PublicFooterProps) {
                         <li>
                             <Link
                                 href="/legal/terms-of-service"
-                                className="text-sm hover:text-primary"
+                                className="text-sm text-neutral-200 hover:text-primary dark:text-neutral-200"
                                 title="Terms of Service"
                             >
                                 Terms of Service
@@ -68,7 +68,7 @@ export default function PublicFooter({ className }: PublicFooterProps) {
                         </li>
                     </ul>
                 </div>
-                <div className="cursor-default text-center text-sm text-neutral-600 dark:text-neutral-200">
+                <div className="cursor-default text-center text-sm text-neutral-200 dark:text-neutral-200">
                     <p>
                         WebDev HQ &middot; All rights reserved &copy;&nbsp;
                         {new Date().getFullYear()}.
