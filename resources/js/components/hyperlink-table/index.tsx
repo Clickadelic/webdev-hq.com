4b177897d117ea8da536283db872b51a15b2b77a
Clickadelic <toby.hopp@gmail.com>
2026-07-02 23:58:57 +0200

    Hyperlink table details

--- resources/js/components/hyperlink-table/index.tsx
+++ resources/js/components/hyperlink-table/index.tsx
@@ -1,4 +1,10 @@
 import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
     Table,
     TableBody,
     TableCell,
@@ -6,9 +12,10 @@ import {
     TableHeader,
     TableRow,
 } from '@/components/ui/table';
-
 import { Hyperlink } from '@/types';
+
 import { usePage } from '@inertiajs/react';
+import { Ellipsis } from 'lucide-react';
 
 export default function HyperlinkTable() {
     const { hyperlinks } = usePage<{ hyperlinks: { data: Hyperlink[] } }>()
@@ -16,13 +23,14 @@ export default function HyperlinkTable() {
     const items = hyperlinks?.data ?? [];
 
     return (
-        <Table className="w-full rounded border">
+        <Table className="w-full">
             <TableHeader>
                 <TableRow>
                     <TableHead>Id</TableHead>
                     <TableHead>Title</TableHead>
                     <TableHead>URL</TableHead>
                     <TableHead>Favicon</TableHead>
+                    <TableHead>Actions</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
@@ -33,6 +41,26 @@ export default function HyperlinkTable() {
                             <TableCell>{link.title}</TableCell>
                             <TableCell>{link.url}</TableCell>
                             <TableCell>{link.favicon_url}</TableCell>
+                            <TableCell>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <button className="flex items-center justify-center rounded-full border p-2 text-muted-foreground transition-colors hover:bg-accent hover:text-accent-foreground focus:ring-2 focus:ring-ring focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50">
+                                            <Ellipsis className="size-4" />
+                                        </button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuItem>
+                                            View
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            Edit
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            Delete
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </TableCell>
                         </TableRow>
                     ))
                 ) : (
