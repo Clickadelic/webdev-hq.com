2679bb47bcdc3151a4726bc9d763524a3ae532bc
Clickadelic <toby.hopp@gmail.com>
2026-07-03 02:08:17 +0200

    URLs corrected

--- resources/js/components/public-header.tsx
+++ resources/js/components/public-header.tsx
@@ -1,11 +1,11 @@
+import AppLogo from '@/components/app-logo';
+import UserDropdownMenu from '@/components/user-dropdown-menu';
+
 import { dashboard, login, register } from '@/routes';
 import { Link, usePage } from '@inertiajs/react';
 
-import AppLogo from '@/components/app-logo';
-
-import { type SharedData } from '@/types';
-
 import { cn } from '@/lib/utils';
+import { type SharedData } from '@/types';
 import { CircleCheckBig, DoorOpen, LayoutDashboard } from 'lucide-react';
 
 interface PublicHeaderProps {
@@ -37,43 +37,46 @@ export default function PublicHeader({
                             </Link>
                         </li>
                     </ul>
-                    {auth.user ? (
-                        <ul className="my-2.5 flex justify-end gap-3">
-                            <li>
-                                <Link
-                                    href={dashboard()}
-                                    className="flex items-center justify-start gap-2 rounded-sm bg-primary px-3 py-1.5 text-sm leading-normal text-white hover:border-[#1915014a] hover:bg-primary/90"
-                                    title="Dashboard"
-                                >
-                                    <LayoutDashboard className="size-4" />
-                                    Dashboard
-                                </Link>
-                            </li>
-                        </ul>
-                    ) : (
-                        <ul className="my-2.5 flex justify-end gap-3">
-                            {canRegister && (
+                    <div className="flex-start flex items-center gap-3">
+                        {auth.user ? (
+                            <ul className="my-2.5 flex justify-end gap-3">
                                 <li>
                                     <Link
-                                        href={register()}
-                                        className="flex items-center justify-between gap-2 rounded-sm border border-slate-200 px-3 py-2 text-slate-800 hover:bg-slate-100 hover:text-primary"
+                                        href={dashboard.url()}
+                                        className="flex items-center justify-start gap-2 rounded-sm bg-primary px-3 py-1.5 text-sm leading-normal text-white hover:border-[#1915014a] hover:bg-primary/90"
+                                        title="Dashboard"
                                     >
-                                        <CircleCheckBig className="size-4" />
-                                        Register
+                                        <LayoutDashboard className="size-4" />
+                                        Dashboard
                                     </Link>
                                 </li>
-                            )}
-                            <li>
-                                <Link
-                                    href={login()}
-                                    className="hover:bg-primary-hover flex items-center justify-between gap-2 rounded-sm border border-slate-200 bg-primary px-3 py-2 text-white hover:bg-primary/90"
-                                >
-                                    <DoorOpen className="size-4" />
-                                    Login
-                                </Link>
-                            </li>
-                        </ul>
-                    )}
+                            </ul>
+                        ) : (
+                            <ul className="my-2.5 flex justify-end gap-3">
+                                {canRegister && (
+                                    <li>
+                                        <Link
+                                            href={register.url()}
+                                            className="flex items-center justify-between gap-2 rounded-sm border border-slate-200 px-3 py-2 text-slate-800 hover:bg-slate-100 hover:text-primary"
+                                        >
+                                            <CircleCheckBig className="size-4" />
+                                            Register
+                                        </Link>
+                                    </li>
+                                )}
+                                <li>
+                                    <Link
+                                        href={login.url()}
+                                        className="hover:bg-primary-hover flex items-center justify-between gap-2 rounded-sm border border-slate-200 bg-primary px-3 py-2 text-white hover:bg-primary/90"
+                                    >
+                                        <DoorOpen className="size-4" />
+                                        Login
+                                    </Link>
+                                </li>
+                            </ul>
+                        )}
+                        <UserDropdownMenu />
+                    </div>
                 </nav>
             </div>
         </header>
