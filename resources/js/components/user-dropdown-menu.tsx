c7500d7adfa531fa571c3a62b7d15a4cbd20e0c1
Clickadelic <toby.hopp@gmail.com>
2026-07-08 00:05:10 +0200

    User dropdown menu

--- resources/js/components/user-dropdown-menu.tsx
+++ resources/js/components/user-dropdown-menu.tsx
@@ -2,38 +2,23 @@ import { Button } from '@/components/ui/button';
 import {
     DropdownMenu,
     DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { UserMenuContent } from '@/components/user-menu-content';
+import { type User } from '@/types';
 
 interface UserDropdownMenuProps {
-    user: {
-        name: string;
-        email: string;
-    };
+    user: User;
 }
+
 export default function UserDropdownMenu({ user }: UserDropdownMenuProps) {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline">{user.name}</Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuGroup>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuItem>{user.name}</DropdownMenuItem>
-                    {/* This is a temporary solution to display the user's email
-                    in the dropdown menu. */}
-                    <DropdownMenuItem>{user.email}</DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                    <DropdownMenuItem>Team</DropdownMenuItem>
-                    <DropdownMenuItem>Subscription</DropdownMenuItem>
-                </DropdownMenuGroup>
+            <DropdownMenuContent align="end">
+                <UserMenuContent user={user} />
             </DropdownMenuContent>
         </DropdownMenu>
     );
